44d47683c9ac60cf2dea99781dcf1c9bde85010e
zombyacoff <zombyacoff@gmail.com>
2024-06-13 21:57:29 +0300

    refactor: use constant from module
    Used constant TELEGRAPH_URL from tuparser module instead of 'telegra.ph' url

--- main.py
+++ main.py
@@ -1,22 +1,22 @@
-from tuparser import TelegraphParser, YAMLOutputFile, Config, run_parser
+from tuparser import TelegraphParser, YAMLOutputFile, Config, run_parser, TELEGRAPH_URL
 
 class LinkParser(TelegraphParser):
     def __init__(self, config, output_file):
         super().__init__(config)
-        self.output_file = output_file   
-           
+        self.output_file = output_file
+
     async def parse(self, url, soup):
-        raw_links = soup.findAll("a", href = True)
+        raw_links = soup.findAll('a', href=True)
         for raw_link in raw_links:
             link = raw_link['href']
             if not link.startswith('mailto:'):
                 if link.startswith('/'):
-                    link = 'https://telegra.ph' + link
-                    self.output_file.write_output((link,url))
+                    link = TELEGRAPH_URL + link
+                    self.output_file.write_output((link, url))
                 else:
-                    self.output_file.write_output((link,url))
-                
-output_file = YAMLOutputFile({"link": {}, "url": {}}, "link-parser-out")
-run_parser(config_class=Config,
-           parser_args=(output_file,),
+                    self.output_file.write_output((link, url))
+
+output_file = YAMLOutputFile({'link': {}, 'url': {}}, 'link-parser-out')
+run_parser(config_class=Config, 
+           parser_args=(output_file,), 
            parser_class=LinkParser)
