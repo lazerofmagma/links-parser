42c74a22c1f9672593f144865eb8efd3b0ec3b17
zombyacoff <zombyacoff@gmail.com>
2024-07-19 18:19:10 +0300

    update tuparser to 0.2.0

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
-from tuparser import TelegraphParser, run_parser, YAMLOutputFile, TELEGRAPH_URL
+from tuparser import TELEGRAPH_URL, TelegraphParser, run_parser
 
 
 class LinkParser(TelegraphParser):
-    def __init__(self, config, output_file):
-        super().__init__(config)
-        self.output_file = output_file
-
     async def parse(self, url, soup):
         links = [link['href'] for link in soup.findAll('a', href=True)]
         for link in links:
@@ -16,5 +12,4 @@ class LinkParser(TelegraphParser):
             self.output_file.write_data(link, url)
 
 
-output_file = YAMLOutputFile({'link': {}, 'url': {}}, 'links-parser-out')
-run_parser(LinkParser, parser_args=[output_file])
+run_parser(LinkParser, titles=[''], output_file=[{'link': {}, 'url': {}}, 'links-parser-out'])
